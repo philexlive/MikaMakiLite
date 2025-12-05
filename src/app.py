f658321431ace78c319dcfe6778b18d28c31806b
philexlive <philex.live@outlook.com>
2025-12-05 16:33:52 +0000

    Code corrected.

--- src/app.py
+++ src/app.py
@@ -2,8 +2,7 @@ import config
 import asyncio
 import telethon as tl
 
-from src.ai.service import client
-from tbl import get_table_from_chat
+from ai.service import client
 
 
 def run_ai():
@@ -11,15 +10,8 @@ def run_ai():
         client.run_until_disconnected()
 
 
-def run_reading():
-    with client:
-        table = get_table_from_chat(config.CHAT, config.TOPIC, client)
-        asyncio.run(table)
-
-
 def main():
     client.start()
-    # run_reading()
     run_ai()
     if client.is_connected:
         client.disconnect()
